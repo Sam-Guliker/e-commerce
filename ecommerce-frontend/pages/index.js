397359843a2b0fb77c16c1095095b9c83ec467f9
Sam-Guliker <samguliker@hotmail.com>
2021-05-21 14:36:33 +0200

    added image util

--- ecommerce-frontend/pages/index.js
+++ ecommerce-frontend/pages/index.js
@@ -1,10 +1,23 @@
 import Layout from "../components/layout";
+import products from "../products.json"
+
+import { getImage } from '../utils/urls'
 
 export default function Home() {
   return (
     <Layout>
       <main className='main'>
-        <p>Home</p>
+        <ul>
+          {products.map((item, key) => {
+            return (
+              <li key={key}>
+                <img src={getImage(item.image)} />
+                <h2>{item.name}</h2>
+                <p>{item.meta_description}</p>
+              </li>
+            )
+          })}
+        </ul>
       </main>
     </Layout>
   )
